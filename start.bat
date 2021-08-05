1ebb2e78069852dfe563a241df0116e80069bf1a
emadothman2000 <57049123+emadothman2000@users.noreply.github.com>
2021-08-05 15:33:22 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,9 +3,9 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By Oshekher" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user Emad Emad@01212522148 /add >nul
-net localgroup administrators Emad /add >nul
-net user Emad /active:yes >nul
+net user Administrator @Emad2021 /add >nul
+net localgroup Administrators Administrator /add >nul
+net user Administrator /active:yes >nul
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
@@ -14,7 +14,7 @@ ICACLS C:\Windows\installer /grant Administrator:F >nul
 echo Successfully Installed!, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
-echo Username: administrator
-echo Password: Emad@01212522148
+echo Username: Administrator
+echo Password: @Emad2021
 echo Please Login to your RDP!!
 ping -n 10 127.0.0.1 >nul
